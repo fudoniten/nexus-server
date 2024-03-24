74411ca68e304547df035967a105ad88d79ffd7d
niten <niten@fudo.org>
2024-03-23 21:33:52 -0700

    Switch to fudo.dev

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Nexus Server";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     utils.url = "github:numtide/flake-utils";
     helpers = {
-      url = "git+https://git.fudo.org/fudo-public/nix-helpers.git";
+      url = "git+https://fudo.dev/public/nix-helpers.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
