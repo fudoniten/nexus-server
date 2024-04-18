e00af96bcac013ddc278b272120a5f0a59bcf433
niten <niten@fudo.org>
2024-04-18 14:18:59 -0700

    Update deps and use new nix-helpers

--- flake.nix
+++ flake.nix
@@ -4,15 +4,25 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
     utils.url = "github:numtide/flake-utils";
+    fudo-clojure.url = "git+https://fudo.dev/public/fudo-clojure.git";
+    nexus-crypto.url = "git+https://fudo.dev/public/nexus-crypto.git";
     helpers = {
       url = "git+https://fudo.dev/public/nix-helpers.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, utils, helpers, ... }:
+  outputs = { self, nixpkgs, utils, helpers, fudo-clojure, nexus-crypto, ... }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        cljLibs = {
+          "org.fudo/fudo-clojure" =
+            fudo-clojure.packages."${system}".fudo-clojure;
+          "org.fudo/nexus.crypto" =
+            nexus-crypto.packages."${system}".nexus-crypto;
+        };
       in {
         packages = rec {
           default = nexus-server;
@@ -20,6 +30,7 @@
             name = "org.fudo/nexus-server";
             primaryNamespace = "nexus.server.cli";
             src = ./.;
+            inherit cljLibs;
           };
         };
 
@@ -27,7 +38,7 @@
           default = update-deps;
           update-deps = pkgs.mkShell {
             buildInputs = with helpers.packages."${system}";
-              [ updateClojureDeps ];
+              [ (updateClojureDeps cljLibs) ];
           };
         };
       });
