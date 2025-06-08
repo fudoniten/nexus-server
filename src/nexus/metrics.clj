ffd86fff9975545f9c57cc94a84c127c98b2bac0
niten <niten@fudo.org>
2025-06-07 17:17:49 -0700

    fix: Use timers/timer instead of metrics/timer

--- src/nexus/metrics.clj
+++ src/nexus/metrics.clj
@@ -19,13 +19,13 @@
 
 (defn time-request [handler]
   (fn [request]
-    (timers/time! (metrics/timer "request-timer")
+    (timers/time! (timers/timer "request-timer")
       (try
         (let [response (handler request)]
           (when-let [req-size (get-in request [:headers "content-length"])]
-            (histograms/update! (metrics/histogram "request-size") (Long/parseLong req-size)))
+            (histograms/update! (histograms/histogram "request-size") (Long/parseLong req-size)))
           (when-let [res-size (get-in response [:headers "content-length"])]
-            (histograms/update! (metrics/histogram "response-size") (Long/parseLong res-size)))
+            (histograms/update! (histograms/histogram "response-size") (Long/parseLong res-size)))
           response)
         (catch Exception e
           (log/warn! e "Error in timed request")
